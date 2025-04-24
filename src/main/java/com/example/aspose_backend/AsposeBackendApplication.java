aaf571990c2bec50aa414a5c22918a03e0afdfb3
vivien <vtitsop4@gmail.com>
2025-04-24 17:15:13 +0100

    Normalisation des fins de ligne

---
+++ src/main/java/com/example/aspose_backend/AsposeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aspose_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsposeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsposeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aspose_backend/AsposeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aspose_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsposeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
